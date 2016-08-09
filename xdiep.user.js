3a45e620402728a69c3938aad38f6648882c6eb7
happyhg3927 <hbhoon0_0@naver.com>
2016-08-09 15:24:35 +0900

    Update xdiep.user.js

--- xdiep.user.js
+++ xdiep.user.js
@@ -1,13 +1,13 @@
 // ==UserScript==
-// @name         xDiep.io (github)
+// @name         Diep.io (github)
 // @description  Diep.io extension (sectored-minimap, zoom, play-with-friends, custom-theming, kill counter)
 // @version      3.0
-// @author       terjanq
+// @author       MungBo
 // @match        http://diep.io/
 // @run-at       document-start
 // @grant        GM_xmlhttpRequest
 // @connect      diep.io
-// @namespace    https://raw.githubusercontent.com/terjanq/xDiep.io/master/xdiep.user.js
+// @namespace    https://raw.githubusercontent.com/happyhg3927/Diep.io/master/xdiep.user.js
 // ==/UserScript==
 
 window.stop();
@@ -16,6 +16,6 @@ GM_xmlhttpRequest({
     method: "GET",
     url: "http://diep.io",
     onload: function(e) {
-         document.open(), document.write("<script src='https://cdn.rawgit.com/terjanq/xDiep.io/master/head.js'></script>" + e.responseText), document.close();
+         document.open(), document.write("<script src='https://cdn.rawgit.com/happyhg3927/Diep.io/master/head.js'></script>" + e.responseText), document.close();
     }
 });
