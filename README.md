e64285ea8d195a89c32fa009264734bc094796c5
happyhg3927 <hbhoon0_0@naver.com>
2016-08-09 10:57:59 +0900

    Added basic scripts & README.md

---
+++ README.md
@@ -0,0 +1,17 @@
+# Diep.io
+Extension for Diep.io game
+
+# Features
+* Sectored minimap
+* Play with friends
+* Custom theming
+* And a lot of more
+
+# Instalation:
+1. You need to use Google Chrome Browser (49+)
+2. Install [Tampermonkey](https://tampermonkey.net/)
+3. Install [xdiep.user.js](https://github.com/terjanq/xDiep.io/raw/master/xdiep.user.js)
+4. Open [diep.io](http://diep.io) tab :)
+
+
+Website: [한국다이피오커뮤니티](http://cafe.naver.com/diepio)
---
+++ xdiep.user.js
@@ -0,0 +1,21 @@
+// ==UserScript==
+// @name         xDiep.io (github)
+// @description  Diep.io extension (sectored-minimap, zoom, play-with-friends, custom-theming, kill counter)
+// @version      3.0
+// @author       terjanq
+// @match        http://diep.io/
+// @run-at       document-start
+// @grant        GM_xmlhttpRequest
+// @connect      diep.io
+// @namespace    https://raw.githubusercontent.com/terjanq/xDiep.io/master/xdiep.user.js
+// ==/UserScript==
+
+window.stop();
+
+GM_xmlhttpRequest({
+    method: "GET",
+    url: "http://diep.io",
+    onload: function(e) {
+         document.open(), document.write("<script src='https://cdn.rawgit.com/terjanq/xDiep.io/master/head.js'></script>" + e.responseText), document.close();
+    }
+});
